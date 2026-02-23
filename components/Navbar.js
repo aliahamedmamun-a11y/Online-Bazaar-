c6a305cdf1f153c037bdfa841bbc6c213262cd51
aliahamedmamun-a11y <aliahamedmamun@gmail.com>
2026-02-23 21:15:26 +0600

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,4 @@
+// components/Navbar.js
 import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
 
@@ -5,12 +6,22 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center p-4 bg-indigo-600 text-white">
       <h2 className="text-xl font-bold">Online Bazaar</h2>
-      <div className="space-x-4">
-        <Link href="/">Home</Link>
-        <Link href="/products">Products</Link>
-        <Link href="/cart">Cart</Link>
-        <Link href="/checkout">Checkout</Link>
-        <Link href="/login">Login</Link>
+      <div className="flex items-center space-x-4">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/products" className="hover:underline">
+          Products
+        </Link>
+        <Link href="/cart" className="hover:underline">
+          Cart
+        </Link>
+        <Link href="/checkout" className="hover:underline">
+          Checkout
+        </Link>
+        <Link href="/login" className="hover:underline">
+          Login
+        </Link>
         <ThemeToggle />
       </div>
     </nav>
