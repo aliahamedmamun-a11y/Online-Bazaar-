626eb48dcd6ca7d2458b44247bea0c250d2ad297
aliahamedmamun-a11y <aliahamedmamun@gmail.com>
2026-02-23 14:32:27 +0600

    Add Products component to display product list

---
+++ pages/products.js
@@ -0,0 +1,25 @@
+
+import { useEffect, useState } from "react";
+
+export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <ul>
+        {products.map((p) => (
+          <li key={p.id}>
+            {p.name} - ${p.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
