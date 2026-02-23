5900bcec22a0d2f385b3cd0521f35f0021446cb6
aliahamedmamun-a11y <aliahamedmamun@gmail.com>
2026-02-23 21:13:18 +0600

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -1,3 +1,4 @@
+// components/Hero.js
 export default function Hero() {
   return (
     <section className="relative h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
