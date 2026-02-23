4902a094613e079f99ece8ec5e3dfdf8eb30c4ef
aliahamedmamun-a11y <aliahamedmamun@gmail.com>
2026-02-23 15:44:39 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Testimonials from "../components/Testimonials";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Testimonials />
     </div>
   );
 }
-
