330aa98547fd172594bccd211e3e1bad5abe49e8
aliahamedmamun-a11y <aliahamedmamun@gmail.com>
2026-02-23 15:25:10 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // Dark/Light mode toggle এর জন্য
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
